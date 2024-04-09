53b832d8fac393ed9b7d7275a4d218a0de413f20
Guilherme Augusto Lopes Campos <1398847@sga.pucminas.br>
2024-04-08 22:01:15 -0300

    enviando exercício de recursividade do tp2

---
+++ recursividade/OMDesceu/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OMDesceu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdLinhas = int.Parse(Console.ReadLine());
+            Recursividade(qtdLinhas);
+        }
+
+        public static void Recursividade(int n)
+        {
+            if(n > 0)
+            {
+                string[] entrada = Console.ReadLine().Split(" ");
+                int x = int.Parse(entrada[0]);
+                int y = int.Parse(entrada[1]);
+                Console.WriteLine(MDC(x,y));
+                Recursividade(n - 1);
+
+            }
+        }
+
+        public static int MDC(int x, int y)
+        {
+            
+            if(x  > y)
+            {
+                return MDC(x - y, y);
+            } else if (x < y)
+            {
+                return MDC(y, x);
+            }
+            return x;
+        }
+    }
+}
