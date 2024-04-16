6f18b07c5752d37949924ab0109c9af443c3d017
guilherme-alc <guilhermealc01@gmail.com>
2024-04-15 22:36:46 -0300

    Adicionar exercício Diamantes e Areia

---
+++ tp-2/DiamantesEAreia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int qtdCasosTeste = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < qtdCasosTeste; i++)
+        {
+            string escavacao = Console.ReadLine();
+            ArrayList ladoDireito = new ArrayList();
+            ArrayList ladoEsquerdo = new ArrayList();
+            int qtdDiamantes = 0;
+            for(int j = 0; j < escavacao.Length; j++)
+            {
+                if (escavacao[j] == '<')
+                {
+                    ladoDireito.Add(escavacao[j]);
+                } else if (escavacao[j] == '>')
+                {
+                    ladoEsquerdo.Add(escavacao[j]);
+                }
+            }
+            if(ladoEsquerdo.Count >= ladoDireito.Count)
+            {
+                qtdDiamantes = ladoDireito.Count;
+            } else
+            {
+                qtdDiamantes = ladoEsquerdo.Count;
+            }
+            Console.WriteLine(qtdDiamantes);
+        }
+    }
+}
+
---
+++ tp-2/DiamantesEAreia/README.md
@@ -0,0 +1,16 @@
+## Diamantes e Areia
+
+João está trabalhando em uma mina, tentando retirar o máximo que consegue de diamantes "<>". Ele deve excluir todas as partículas de areia "." do processo e a cada retirada de diamante, novos diamantes poderão se formar.
+
+Se ele tem uma entrada .<...<<..>>....>....>>>., três diamantes são formados. O primeiro é retirado de <..>, resultando  .<...<>....>....>>>.
+Em seguida o segundo diamante é retirado, restando .<.......>....>>>.
+O terceiro diamante é então retirado, restando no final .....>>>., sem possibilidade de extração de novo diamante.  
+
+ 
+
+### Entrada
+Deve ser lido um valor inteiro N que representa a quantidade de casos de teste. Cada linha a seguir é um caso de teste que contém até 1000 caracteres, incluindo "<,>, ."
+
+
+### Saída
+Você deve imprimir a quantidade de diamantes possíveis de serem extraídos em cada caso de entrada.
