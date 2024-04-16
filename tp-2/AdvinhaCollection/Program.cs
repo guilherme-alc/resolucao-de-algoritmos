6aee68daaa467fcc11333fd542689e78a3d6fa45
guilherme-alc <guilhermealc01@gmail.com>
2024-04-15 21:13:22 -0300

    Adicionar exércicio de adivinhar tipo de coleção pelas entradas e saídas de dados

---
+++ tp-2/AdvinhaCollection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int qtdCasoTeste = 1;
+        while (qtdCasoTeste != 0)
+        {
+            qtdCasoTeste = Convert.ToInt16(Console.ReadLine());
+            List<int> numerosEntrada = new List<int>();
+            List<int> numerosSaida = new List<int>();
+            for (int i = 0; i < qtdCasoTeste; i++)
+            {
+                string[] instrucao = Console.ReadLine().Split(" ");
+                string comando = instrucao[0];
+                int numero = Convert.ToInt16(instrucao[1]);
+                if (comando == "I")
+                {                        
+                    numerosEntrada.Add(numero);
+                } else if (comando == "R") 
+                {
+                    numerosSaida.Add(numero);
+                }
+            }
+            if(qtdCasoTeste != 0)
+            {
+                string estrutua = "";
+                if (numerosEntrada[0] == numerosSaida[0])
+                {
+                    estrutua = "queue";
+                } else if (numerosEntrada[0] == numerosSaida[numerosSaida.Count -1] && numerosSaida[0] == numerosEntrada[numerosEntrada.Count -1])
+                {
+                    estrutua = "stack";
+                } else
+                {
+                    estrutua = "list";
+                }
+                Console.WriteLine(estrutua);
+            }
+        }
+    }
+}
