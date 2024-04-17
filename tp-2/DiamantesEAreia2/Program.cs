3db05c5c17de25da0b14c9d848f3bbca4f767f90
guilherme-alc <guilhermealc01@gmail.com>
2024-04-17 08:52:57 -0300

    Adicionando nova versão para de DiamantesEAreia

---
+++ tp-2/DiamantesEAreia2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System;
+namespace DiamantesEAreia2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdTestes = int.Parse(Console.ReadLine());
+            while(qtdTestes > 0)
+            {
+                string entrada = Console.ReadLine().Replace(".", "");
+                Stack partesDiamantes = new Stack();
+                int diamantesFormados = 0;
+                for(int i = 0; i < entrada.Length; i++)
+                {
+                    if (entrada[i] == '<')
+                    {
+                        partesDiamantes.Push(entrada[i]);
+                    } else if (entrada[i] == '>' && partesDiamantes.Contains('<'))
+                    {
+                        diamantesFormados++;
+                        partesDiamantes.Pop();
+                    }
+                }
+                Console.WriteLine(diamantesFormados);
+                qtdTestes--;
+            }
+        }
+    }
+}
