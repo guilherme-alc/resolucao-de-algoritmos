85b342e93968e4336b2a78cd2eaa03699e5d314d
guilherme-alc <guilhermealc01@gmail.com>
2024-04-23 15:35:30 -0300

    Refatorando forma de inicializar o Dictionary com valores e completando a solução

--- collections/tp/Questao33/Program.cs
+++ collections/tp/Questao33/Program.cs
@@ -5,28 +5,34 @@ namespace Questao33
     {
         static void Main(string[] args)
         {
-            //33 – Faça um programa que monte a estrutura abaixo usando Dictionary<> ou SortedList<>:
-            Dictionary<string, double> brasil = new Dictionary<string, double>() { ["Brasil"] = 202656784 };
-            Dictionary<string, double> mexico = new Dictionary<string, double>() { ["México"] = 120286656 };
-            Dictionary<string, double> eua = new Dictionary<string, double>() { ["Estados Unidos"] = 318892096 };
-            Dictionary<string, double> guatemala = new Dictionary<string, double>() { {"Guatemala", 17153288 } };
-            Dictionary<string, double> equador = new Dictionary<string, double>() { {"Equador", 16904868 } };
-            Dictionary<string, double> colombia = new Dictionary<string, double>() { {"Colombia", 49084840 } };
-
-            List<Dictionary<string, double>> paisesAmericanos = new List<Dictionary<string, double>>();
-            paisesAmericanos.Add(brasil);
-            paisesAmericanos.Add(mexico);
-            paisesAmericanos.Add(eua);
-            paisesAmericanos.Add(guatemala);
-            paisesAmericanos.Add(equador);
-            paisesAmericanos.Add(colombia);
+            //33 – Faça um programa que monte a estrutura abaixo usando Dictionary<> ou SortedList<>:,
 
             Dictionary<string, List<Dictionary<string, double>>> continentes = new Dictionary<string, List<Dictionary<string, double>>>();
-            continentes.Add("America", paisesAmericanos);
+
+            continentes.Add("America", new List<Dictionary<string, double>>());
+            continentes["America"].Add(new Dictionary<string, double>() { { "Brasil", 202656784 } });
+            continentes["America"].Add(new Dictionary<string, double>() { { "México", 120286656 } });
+            continentes["America"].Add(new Dictionary<string, double>() { { "Estados Unidos", 318892096 } });
+            continentes["America"].Add(new Dictionary<string, double>() { { "Guatemala", 17153288 } });
+            continentes["America"].Add(new Dictionary<string, double>() { { "Equador", 16904868 } });
+            continentes["America"].Add(new Dictionary<string, double>() { { "Colombia", 49084840 } });
+            continentes.Add("Ásia", new List<Dictionary<string, double>>());
+            continentes["Ásia"].Add(new Dictionary<string, double>() { { "China", 1355692544 } });
+            continentes["Ásia"].Add(new Dictionary<string, double> (){ { "Japão", 127103392 } });
+            continentes["Ásia"].Add(new Dictionary<string, double> () { { "Rússia", 141722208 } });
+            continentes["Ásia"].Add(new Dictionary<string, double>() { { "Tailândia", 68977400 } });
+            continentes["Ásia"].Add(new Dictionary<string, double>() { { "Líbano", 5469612 } });
+            continentes["Ásia"].Add(new Dictionary<string, double>() { {"Índia", 1326093184} });
+            continentes.Add("África", new List<Dictionary<string, double>>());
+            continentes["África"].Add(new Dictionary<string, double>() { {"Nigéria", 214028304} });
+            continentes["África"].Add(new Dictionary<string, double>() { {"Angola", 32522340} });
+            continentes["África"].Add(new Dictionary<string, double>() { {"Egito", 104124440} });
+            continentes["África"].Add(new Dictionary<string, double>() { {"África do Sul", 56463616} });
+            continentes["África"].Add(new Dictionary<string, double>() { {"Marrocos", 35561656} });
 
             foreach(string chave in continentes.Keys)
             {
-                Console.WriteLine($"Continente: {chave}");
+                Console.WriteLine($"Continente: {chave}\n");
                 List<Dictionary<string, double>> continente = continentes[chave];
                 double somaPopulacaoTotal = 0;
                 for(int i = 0; i < continente.Count; i++ )
@@ -38,11 +44,9 @@ namespace Questao33
                         somaPopulacaoTotal += paisInfo.Value;
                     }
                 }
-                Console.WriteLine($"População total: {somaPopulacaoTotal}");
+                Console.WriteLine($"\nPopulação total: {somaPopulacaoTotal}\n");
+                Console.WriteLine();
             }
-            //esse foi hard, tive que comentar. descobri 2 formas de inicializar um dicionário já com chaves e valor
-            //quebrei cabeça pra entender que os valores do dicionário continentes tinha que ser uma lista
-            //a sacada que mais curti foi percorrer os continentes com foreach e continentes.key
         }
     }
 }
