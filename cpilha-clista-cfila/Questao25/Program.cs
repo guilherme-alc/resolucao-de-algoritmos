02a9ad33f9993ff6c82ce89de3f35e189f2b0e98
Guilherme Campos <guilhermealc01@gmail.com>
2024-06-05 17:42:38 -0300

    solução Questao25

--- cpilha-clista-cfila/Questao25/Program.cs
+++ cpilha-clista-cfila/Questao25/Program.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+    internal class Celula
+    {
+        public Object Item;
+        public Celula Prox;
+        public Celula()
+        {
+            Item = null;
+            Prox = null;
+        }
+        public Celula(Object valorItem)
+        {
+            Item = valorItem;
+            Prox = null;
+        }
+        public Celula(Object valorItem, Celula valorProx)
+        {
+            Item = valorItem;
+            Prox = valorProx;
+        }
+    }
+    internal class CLista
+    {
+        private Celula primeira;
+        private Celula ultima;
+        private int qtde;
+        public CLista()
+        {
+            primeira = new Celula();
+            ultima = primeira;
+            qtde = 0;
+        }
+        public void RemoverApos(Object item)
+        {
+            Celula aux = primeira.Prox;
+            bool achou = false;
+            int count = 1;
+            if(primeira != ultima)
+            {
+                while(aux != null && !achou)
+                {
+                    if(aux.Item.Equals(item))
+                    {
+                        achou = true;
+                        ultima = aux;
+                        ultima.Prox = null;
+                        qtde = count;
+                        
+                    } else
+                    {
+                        aux = aux.Prox;
+                        count++;
+                    }
+                }
+            } else
+            {
+                Console.WriteLine("A lista está vazia.");
+            }
         }
     }
 }
